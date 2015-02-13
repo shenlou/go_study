b5870677367a9a376944f5c0216ad30a54e5f819
shenlou <beyondjinbing@qq.com>
2015-02-13 09:22:43 +0800

    lng天气预报日志处理，若没有捕获成功的消息，则发邮件提醒业务人员

--- lngweatherlog/handlelog.go
+++ lngweatherlog/handlelog.go
@@ -8,50 +8,78 @@ import (
 	"log"
 	"net/smtp"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
+type ConfigInfo struct {
+	logPath string
+	mailConfig
+	timeConfig
+}
+
+type mailConfig struct {
+	username, password, host, to string
+}
+
+type timeConfig struct {
+	from, to string
+}
+
 //读取配置文件
-func getLogFromIni() (string, string, string, string, string) {
+func getLogFromIni() *ConfigInfo {
 	configFile := "config.ini"
 	key := "logPath"
 	section := "mail"
 	config, err := goconfig.LoadConfigFile(configFile)
 	if err != nil {
-		log.Fatalf("载入配置文件%s错误", configFile)
+		log.Fatalf("载入配置文件%s错误,%s", configFile, err.Error())
 	}
 	logPath, err := config.GetValue(goconfig.DEFAULT_SECTION, key)
 	if err != nil {
-		log.Fatalf("读取节点%s错误", key)
+		log.Fatalf("读取节点%s错误,%s", key, err.Error())
 	}
 	username, err := config.GetValue(section, "username")
 	if err != nil {
-		log.Fatalf("读取节点%s错误", "username")
+		log.Fatalf("读取节点%s错误%s", "username", err.Error())
 	}
 	password, err := config.GetValue(section, "password")
 	if err != nil {
-		log.Fatalf("读取节点%s错误", "password")
+		log.Fatalf("读取节点%s错误%s", "password", err.Error())
 	}
 	host, err := config.GetValue(section, "host")
 	if err != nil {
-		log.Fatalf("读取节点%s错误", "host")
+		log.Fatalf("读取节点%s错误%s", "host", err.Error())
 	}
 	to, err := config.GetValue(section, "to")
 	if err != nil {
-		log.Fatalf("读取节点%s错误", "to")
+		log.Fatalf("读取节点%s错误%s", "to", err.Error())
 	}
-	return logPath, username, password, host, to
+	timeFrom, err := config.GetValue("time", "from")
+	if err != nil {
+		log.Fatalf("读取节点%s错误%s,", "from", err.Error())
+	}
+	timeTo, err := config.GetValue("time", "to")
+	if err != nil {
+		log.Fatalf("读取节点%s错误%s,", "time.to", err.Error())
+	}
+
+	return &ConfigInfo{
+		logPath:    logPath,
+		mailConfig: mailConfig{username, password, host, to},
+		timeConfig: timeConfig{timeFrom, timeTo},
+	}
 }
 
 //处理lngweather日志
 func handleLngweatherLog() {
-	fileName, username, password, host, to := getLogFromIni()
-	file, err := os.Open(fileName)
-	defer file.Close()
+	config := getLogFromIni()
+	file, err := os.Open(config.logPath)
 	if err != nil {
-		log.Fatalf("读入文件%s出错", fileName)
+		log.Fatalf("读入文件%s出错,%s", config.logPath, err.Error())
 	}
+	defer file.Close()
 	buff := bufio.NewReader(file)
 	decode := mahonia.NewDecoder("gbk")
 	nowDate := time.Now().Format("2006-01-02")
@@ -69,7 +97,8 @@ func handleLngweatherLog() {
 	}
 	if a == 0 {
 		errMsg := "LNG青岛接收站" + nowDate + "天气预报邮件发送失败！"
-		sendLngErrLog(username, password, host, to, errMsg)
+		log.Println(errMsg)
+		sendLngErrLog(config.username, config.password, config.host, config.mailConfig.to, errMsg)
 	} else {
 		log.Println(successMsg)
 	}
@@ -77,14 +106,23 @@ func handleLngweatherLog() {
 
 //定时器
 func lngTimer() {
-	timer := time.NewTicker(10 * time.Hour)
+	config := getLogFromIni()
+	timeFrom, err := strconv.Atoi(config.from)
+	if err != nil {
+		log.Fatalf("timeFrom类型转化错误,%s", err.Error())
+	}
+	timeTo, err := strconv.Atoi(config.timeConfig.to)
+	if err != nil {
+		log.Fatalf("timeTo类型转换错误,%s", err.Error())
+	}
+	timer := time.NewTicker(10 * time.Second)
 	for {
 		select {
 		case <-timer.C:
 			now := time.Now()
-			beforeTime := time.Date(now.Year(), now.Month(), now.Day(), 15, 0, 0, 0, now.Location())
-			afterTime := time.Date(now.Year(), now.Month(), now.Day(), 16, 0, 0, 0, now.Location())
-			if now.After(beforeTime) && now.Before(afterTime) {
+			fromTime := time.Date(now.Year(), now.Month(), now.Day(), timeFrom, 0, 0, 0, now.Location())
+			toTime := time.Date(now.Year(), now.Month(), now.Day(), timeTo, 0, 0, 0, now.Location())
+			if now.After(fromTime) && now.Before(toTime) {
 				handleLngweatherLog()
 			}
 		}
@@ -98,7 +136,10 @@ func sendLngErrLog(username, password, host, to, body string) {
 	content_type := "Content-Type: text/plain; charset=UTF-8"
 	msg := []byte("To: " + to + "\r\nFrom: " + username + "\r\nSubject: lngweather发送失败\r\n" + content_type + "\r\n\r\n" + body)
 	send_to := strings.Split(to, ";")
-	smtp.SendMail(host, auth, username, send_to, msg)
+	err := smtp.SendMail(host, auth, username, send_to, msg)
+	if err != nil {
+		log.Fatalf("邮件发送失败,%s", err.Error())
+	}
 }
 
 func main() {
